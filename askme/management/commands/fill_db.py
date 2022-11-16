c459a3f7e7944630a3799e3db0cf5b6ec1893b82
puzzzik <vtazenkovv@yandex.ru>
2022-11-16 19:09:53 +0300

    fixed fill_db

--- askme/management/commands/fill_db.py
+++ askme/management/commands/fill_db.py
@@ -21,88 +21,88 @@ class Command(BaseCommand):
 
         # теги
 
-        # try:
-        #     [Tag(text=f'tag №{i}').save() for i in range(1, ratio+1)]
-        # except Exception:
-        #     raise CommandError(Exception)
-        # self.stdout.write(self.style.SUCCESS(
-        #     'Successfully filled tags in database'))
+        try:
+            [Tag(text=f'tag №{i}').save() for i in range(1, ratio+1)]
+        except Exception:
+            raise CommandError(Exception)
+        self.stdout.write(self.style.SUCCESS(
+            'Successfully filled tags in database'))
 
         # профили
 
-        # try:
-        #     users = []
-        #     for i in range(1, ratio*10 + 1):
-        #         user = User(username=f'user {i}',
-        #                     first_name=f'name {i}',
-        #                     last_name=f'last name {i}',
-        #                     email=f'email {i}',
-        #                     password=f'password {i}')
-        #         user.save()
-        # except Exception as e:
-        #     logging.exception(e)
-        #     print('profiles failed')
-        #     raise CommandError(Exception)
-        # self.stdout.write(self.style.SUCCESS(
-        #     'Successfully filled users in database'))
+        try:
+            users = []
+            for i in range(1, ratio*10 + 1):
+                user = User(username=f'user {i}',
+                            first_name=f'name {i}',
+                            last_name=f'last name {i}',
+                            email=f'email {i}',
+                            password=f'password {i}')
+                user.save()
+        except Exception as e:
+            logging.exception(e)
+            print('profiles failed')
+            raise CommandError(Exception)
+        self.stdout.write(self.style.SUCCESS(
+            'Successfully filled users in database'))
 
         # лайки
 
-        # try:
-        #     [
-        #         Like(
-        #             like=bool(randint(0, 1)),
-        #             user=Profile.objects.get(pk=randint(1, ratio*10))
-        #         ).save() for i in range(1, ratio*200+1)]
+        try:
+            [
+                Like(
+                    like=bool(randint(0, 1)),
+                    user=Profile.objects.get(pk=randint(1, ratio*10))
+                ).save() for i in range(1, ratio*200+1)]
 
-        # except Exception:
-        #     print('likes failed')
-        #     raise CommandError(Exception)
-        # self.stdout.write(self.style.SUCCESS(
-        #     'Successfully filled likes in database'))
+        except Exception:
+            print('likes failed')
+            raise CommandError(Exception)
+        self.stdout.write(self.style.SUCCESS(
+            'Successfully filled likes in database'))
 
         # вопросы
 
-        # left = 1
-        # right = 0
-        # try:
-        #
-        #     for i in range(1, ratio*10 + 1):
+        left = 1
+        right = 0
+        try:
+        
+            for i in range(1, ratio*10 + 1):
 
-        #         tags = [Tag.objects.get(pk=randint(1, ratio))
-        #                 for i in range(randint(1, 5))]
+                tags = [Tag.objects.get(pk=randint(1, ratio))
+                        for i in range(randint(1, 5))]
 
-        #         right += randint(1, 10)
-        #         likes = [Like.objects.get(pk=i)
-        #                  for i in range(left, right)]
-        #         left = right
+                right += randint(1, 10)
+                likes = [Like.objects.get(pk=i)
+                         for i in range(left, right)]
+                left = right
 
-        #         question = Question(
-        #             author=Profile.objects.get(pk=randint(1, ratio*10)),
-        #             title=f'title {i}',
-        #             text=f'text {i}',
-        #         )
+                question = Question(
+                    author=Profile.objects.get(pk=randint(1, ratio*10)),
+                    title=f'title {i}',
+                    text=f'text {i}',
+                )
 
-        #         rating = 0
-        #         for like in likes:
-        #             rating += like.like
+                rating = 0
+                for like in likes:
+                    rating += like.like
 
-        #         question.save()
-        #         question.tags.set(tags)
-        #         question.likes.set(likes)
-        #         question.rating = rating
-        #         question.save()
+                question.save()
+                question.tags.set(tags)
+                question.likes.set(likes)
+                question.rating = rating
+                question.save()
 
-        # except Exception:
-        #     print('questions failed')
-        #     raise CommandError(Exception)
-        # self.stdout.write(self.style.SUCCESS(
-        #     'Successfully filled questions in database'))
+        except Exception:
+            print('questions failed')
+            raise CommandError(Exception)
+        self.stdout.write(self.style.SUCCESS(
+            'Successfully filled questions in database'))
 
         # ответы
 
         try:
-            for i in range(105_523, 1_000_000 + 1):
+            for i in range(1, ratio*100 + 1):
 
                 likes = [Like(like=bool(randint(0, 1)),
                               user=Profile.objects.get(pk=randint(1, ratio*10))
